de4d1265c6955aed6601b71a0a9dc230766ac9ad
chanuka <zorojurio@gmail.com>
2023-09-19 12:36:30 +0530

    change settings

--- autocompany/settings.py
+++ autocompany/settings.py
@@ -84,13 +84,8 @@ WSGI_APPLICATION = 'autocompany.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': POSTGRES_DB_NAME,
-        'USER': POSTGRES_USER_NAME,
-        'PASSWORD': POSTGRES_PASSWORD,
-        'HOST': POSTGRES_HOST,
-        'PORT': POSTGRES_PORT,
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
